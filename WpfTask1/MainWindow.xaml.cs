79daeb8f18366ad0a0ba5298a43e9888935a6d63
Ibrahim <alizadaibrahimm07@gmail.com>
2023-10-12 20:53:23 +0400

    WPF TASK 1

---
+++ WpfTask1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfTask1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    void btnRight_Click(object sender, MouseButtonEventArgs e)
+    {
+        Button btn = (Button)sender;
+        btn.Visibility = Visibility.Collapsed;
+    }
+
+    void btn_Click(object sender, RoutedEventArgs e)
+    {
+        Button btn = (Button)sender;
+
+        int r = Random.Shared.Next(0, 255);
+        int g = Random.Shared.Next(0, 255);
+        int b = Random.Shared.Next(0, 255);
+
+        btn.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+
+        MessageBox.Show(btn.Name, "Button Information", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
